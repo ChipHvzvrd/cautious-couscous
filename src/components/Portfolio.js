c6c0108bd407c97008fc7832bebed48f19b6c1b7
Christian Pena <Chip@Christians-MBP.attlocal.net>
2022-12-01 23:04:01 -0600

    init commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from '../assets/images/gh-logo-2.png';
+
+function Portfolio() {
+    return (
+        <div class ="grid grid-cols-4 gap-4"> 
+        </div>
+    );
+}
+
+export default Portfolio;
